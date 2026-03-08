8b661fac235d019045dcdaa61b0c1ba87adb1148
emixsd <emily.dias.hs@gmail.com>
2026-03-08 20:25:01 -0300

    Fix formatting issue in webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -13,7 +13,7 @@ const TAG_TO_ACTION = {
   'p-indecx3': 'CKEPEXUP',
   'p-indecx4': 'NKISR8O1',
   'p-indecx5': '8OVWL4UE',
-  'p-indecx6': 'ZWM2SC7X'
+  'p-indecx6': 'ZWM2SC7X',
   'p-indecx7': 'OEOK5BH9'
 };
 
